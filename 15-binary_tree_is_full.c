3055c9f470cc586435408e6f47b7239ca8e5b8ff
Kariuki11 <irungukenneth433@gmail.com>
2024-03-27 01:42:41 +0300

    This file has disturbed me alot

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,10 +13,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->right && !tree->left)
+	if (!tree->left && !tree->right)
 		return (1);
 
-	if (tree->right && tree->left)
+	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 
