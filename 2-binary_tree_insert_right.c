8eef7adfba873c7ec286daaeb37adbb3608267aa
kariuki11 <irungukenneth433@gmail.com>
2024-03-26 04:43:52 -0700

    The binary tree files

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,26 +11,26 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent)
 		return (NULL);
-	
+
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
-	
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-	
+
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;
 		new->right->parent = new;
 	}
-	
+
 	parent->right = new;
-	
+
 	return (new);
 }
